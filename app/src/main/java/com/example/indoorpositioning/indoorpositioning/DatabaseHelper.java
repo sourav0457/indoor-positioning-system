88173b1e33ef72e1ee8b84f023dfe1d548318acb
sourav0457 <sourav0457@gmail.com>
2017-04-19 20:24:48 +0530

    40% done

--- app/src/main/java/com/example/indoorpositioning/indoorpositioning/DatabaseHelper.java
+++ app/src/main/java/com/example/indoorpositioning/indoorpositioning/DatabaseHelper.java
@@ -11,7 +11,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper{
+
     public static final String DATABASE_NAME = "TrainingSet.db";
+
+    //Column Names for selectLocation class table
     public static final String TABLE_NAME = "PrimaryLocation";
     public static final String ID = "ID";
     public static final String LOCATION_NAME = "LOCATION_NAME";
@@ -19,22 +22,60 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String MAC_2 = "MAC_ADDRESS_2";
     public static final String MAC_3 = "MAC_ADDRESS_3";
 
+    //Column Names for TrainingSet class table
+    public static final String TABLE_NAME_TRAINING = "TrainingSet";
+    public static final String TRAINING_ID = "TRAINING_ID";
+    public static final String X_VALUE = "X";
+    public static final String Y_VALUE = "Y";
+    public static final String RSS_M1 = "M1";
+    public static final String RSS_M2 = "M2";
+    public static final String RSS_M3 = "M3";
+
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, 5);
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+ LOCATION_NAME + " TEXT, " + MAC_1 +" TEXT, "+ MAC_2 +" TEXT, "+ MAC_3 +" TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+ LOCATION_NAME + " TEXT, " + MAC_1 +" TEXT, "+ MAC_2 +" TEXT, "+ MAC_3 +" TEXT);");
+        //db.execSQL("CREATE TABLE " + TABLE_NAME_TRAINING + " (" + TRAINING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+ X_VALUE + " INTEGER, " + Y_VALUE +" INTEGER, "+ RSS_M1 +" INTEGER, "+ RSS_M2 +" INTEGER, "+ RSS_M3 +" INTEGER, "+ ID +" INTEGER);");
+        db.execSQL("create table " + TABLE_NAME_TRAINING + " (" + TRAINING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+ X_VALUE + " INTEGER, " + Y_VALUE +" INTEGER, "+ RSS_M1 +" INTEGER, "+ RSS_M2 +" INTEGER, "+ RSS_M3 +" INTEGER, "+ ID +" INTEGER, " + " FOREIGN KEY ("+ID+") REFERENCES "+TABLE_NAME+"("+ID+"));");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_TRAINING);
         onCreate(db);
     }
+
+    //METHODS FOR TrainingSet class TABLES
+    public boolean insertDataTraining(int x, int y, int rss_m1, int rss_m2, int rss_m3, int i){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(X_VALUE, x);
+        contentValues.put(Y_VALUE, y);
+        contentValues.put(RSS_M1, rss_m1);
+        contentValues.put(RSS_M2, rss_m2);
+        contentValues.put(RSS_M3, rss_m3);
+        contentValues.put(ID, i);
+        long result = db.insert(TABLE_NAME_TRAINING, null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllDataTraining(int i){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT " + X_VALUE + " , " + Y_VALUE + " FROM " + TABLE_NAME_TRAINING + " WHERE " + ID + " = " + i , null);
+        //Cursor res = db.rawQuery("SELECT " + X_VALUE + " , " + Y_VALUE + " FROM " + TABLE_NAME_TRAINING + " WHERE " + ID + " = " + i , null);
+        return res;
+    }
+
+    //METHODS FOR selectLocation class TABLES
 
     public int deleteData(int id){
         SQLiteDatabase db = this.getWritableDatabase();
--- app/src/main/java/com/example/indoorpositioning/indoorpositioning/TrainingSet.java
+++ app/src/main/java/com/example/indoorpositioning/indoorpositioning/TrainingSet.java
@@ -4,9 +4,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.database.SQLException;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,62 +16,55 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-
-public class selectLocation extends AppCompatActivity {
+public class TrainingSet extends AppCompatActivity {
 
     CoordinatorLayout mcoordinatorLayout;
-    ListView lv;
     DatabaseHelper myDb;
+    GridView gv;
     AlertDialog alertDialog;
-    EditText locationName;
-    EditText macaddr1;
-    EditText macaddr2;
-    EditText macaddr3;
+    EditText xparam;
+    EditText yparam;
+    int intentId;
 
     public void displaydata(){
-        final Cursor res = myDb.getAllData();
-        lv = (ListView) findViewById(R.id.idListView);
+        Cursor res = myDb.getAllDataTraining(intentId);
+        gv = (GridView)findViewById(R.id.idGridView);
         if(res.getCount() == 0){
-            Snackbar snackbar = Snackbar.make(mcoordinatorLayout,"Add Locations To Get Started", Snackbar.LENGTH_LONG);
+            Snackbar snackbar = Snackbar.make(mcoordinatorLayout,"Add Training Data To Get Started", Snackbar.LENGTH_LONG);
             snackbar.show();
             return;
         }
 
-        final ArrayList<Integer> arrayId = new ArrayList<>();
         ArrayList<String> buffer = new ArrayList<>();
         res.moveToFirst();
         try {
             do {
-                arrayId.add(res.getInt(0));
-                buffer.add(res.getString(1));
+                buffer.add("( X : " + res.getInt(0) + ", Y : " + res.getInt(1) + " )");
             }while(res.moveToNext());
         }
         finally {
             res.close();
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,buffer);
-        lv.setAdapter(adapter);
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+        gv.setAdapter(adapter);
+        /*gv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                AlertDialog deletealert = new AlertDialog.Builder(selectLocation.this).create();
+                AlertDialog deletealert = new AlertDialog.Builder(TrainingSet.this).create();
                 deletealert.setTitle("Delete");
                 deletealert.setMessage("\nConfirm Delete?");
                 deletealert.setButton(Dialog.BUTTON_POSITIVE,"YES",new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which){
                         int id, rowsAffected;
-                        Cursor res = myDb.getAllData();
+                        Cursor res = myDb.getAllDataPrimary();
                         res.moveToPosition(position);
                         id = res.getInt(0);
                         rowsAffected = myDb.deleteData(id);
@@ -83,7 +75,7 @@ public class selectLocation extends AppCompatActivity {
                             Intent intent = getIntent();
                             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                             finish();
-                            //Removes animation from switching from one activity to another
+                            //Removes animation when switching from one activity to another
                             overridePendingTransition(0,0);
                             startActivity(intent);
                             Toast.makeText(getApplicationContext(),"Successfully Deleted Item", Toast.LENGTH_SHORT).show();
@@ -99,95 +91,98 @@ public class selectLocation extends AppCompatActivity {
                     }
                 });
                 deletealert.show();
-                return true;
+                return false;
             }
-        });
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        });*/
+
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-                Toast.makeText(getApplicationContext(),"Item Clicked: "+ arrayId.get(i),Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), TrainingSet.class);
-                intent.putExtra("id", arrayId.get(i));
-                startActivity(intent);
 
+                //Enter code to call another activity here
+                Toast.makeText(getApplicationContext(),"Item Clicked: "+i,Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
     public void createAlertBox() {
-        alertDialog = new AlertDialog.Builder(selectLocation.this).create();
-        alertDialog.setTitle("Add a new Location");
+        alertDialog = new AlertDialog.Builder(TrainingSet.this).create();
+        alertDialog.setTitle("Enter Your X and Y Co-ordinates");
         //alertDialog.setMessage("\nEnter Location Name");
 
         LinearLayout layout = new LinearLayout(getApplicationContext());
         layout.setOrientation(LinearLayout.VERTICAL);
 
-        locationName = new EditText(selectLocation.this);
-        locationName.setHint("Enter Location Name:");
-        locationName.setGravity(Gravity.LEFT);
-        layout.addView(locationName);
+        xparam = new EditText(TrainingSet.this);
+        xparam.setHint("X Co-ordinate");
+        xparam.setGravity(Gravity.LEFT);
+        layout.addView(xparam);
 
-        macaddr1 = new EditText(selectLocation.this);
-        macaddr1.setHint("Enter MAC Address 1:");
-        macaddr1.setGravity(Gravity.LEFT);
-        layout.addView(macaddr1);
-
-        macaddr2 = new EditText(selectLocation.this);
-        macaddr2.setHint("Enter MAC Address 2:");
-        macaddr2.setGravity(Gravity.LEFT);
-        layout.addView(macaddr2);
-
-        macaddr3 = new EditText(selectLocation.this);
-        macaddr3.setHint("Enter MAC Address 3:");
-        macaddr3.setGravity(Gravity.LEFT);
-        layout.addView(macaddr3);
+        yparam = new EditText(TrainingSet.this);
+        yparam.setHint("Y Co-ordinate");
+        yparam.setGravity(Gravity.LEFT);
+        layout.addView(yparam);
 
         alertDialog.setView(layout);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_location);
-        mcoordinatorLayout = (CoordinatorLayout) findViewById(R.id.idLayout);
-        myDb = new DatabaseHelper(this);
+        setContentView(R.layout.activity_training_set);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mcoordinatorLayout = (CoordinatorLayout) findViewById(R.id.idLayout);
+        myDb = new DatabaseHelper(this);
+
+        Intent intent = getIntent();
+        intentId = intent.getIntExtra("id", 0);
 
         displaydata();
 
+        Toast.makeText(getApplicationContext(),"Training Set! ",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),"Item Clicked: "+ intentId,Toast.LENGTH_SHORT).show();
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
                 createAlertBox();
-
 
                 alertDialog.setButton(Dialog.BUTTON_POSITIVE,"ADD",new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String name = locationName.getText().toString().trim();
-                        String mac1 = macaddr1.getText().toString().trim();
-                        String mac2 = macaddr2.getText().toString().trim();
-                        String mac3 = macaddr3.getText().toString().trim();
-                        if(name.equals("") || mac1.equals("") || mac2.equals("") || mac3.equals("")){
+                        String xvalue = xparam.getText().toString().trim();
+                        String yvalue = yparam.getText().toString().trim();
+                        if(xvalue.equals("") || yvalue.equals("")){
                             Toast.makeText(getApplicationContext(),"Please Fill All Fields", Toast.LENGTH_LONG).show();
                             return;
                         }
                         else
                         {
-                            boolean isInserted = myDb.insertData(name,mac1,mac2,mac3);
-                            if(isInserted == true) {
-                                Intent intent = getIntent();
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                                finish();
-                                overridePendingTransition(0,0);
-                                startActivity(intent);
-                                Toast.makeText(getApplicationContext(), "Saved Successfully", Toast.LENGTH_SHORT).show();
+                            int xcoordinate = Integer.parseInt(xvalue);
+                            int ycoordinate = Integer.parseInt(yvalue);
+
+                            try {
+                                boolean isInserted = myDb.insertDataTraining(xcoordinate, ycoordinate, -9, -8, -7, intentId);
+                                if (isInserted == true) {
+                                    Intent intentrefresh = getIntent();
+                                    intentrefresh.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                    finish();
+                                    overridePendingTransition(0, 0);
+                                    startActivity(intentrefresh);
+                                    Toast.makeText(getApplicationContext(), "Saved Successfully", Toast.LENGTH_LONG).show();
+                                } else
+                                    Toast.makeText(getApplicationContext(), "Error: Could Not Insert Data", Toast.LENGTH_LONG).show();
                             }
-                            else
-                                Toast.makeText(getApplicationContext(),"Error: Could Not Insert Data", Toast.LENGTH_LONG).show();
+                            catch (Exception e){
+                                Toast.makeText(getApplicationContext(), "Something was wrong: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+
                         }
-                        myDb.close();
+                        //myDb.close();
                         //finish();
                     }
                 });
